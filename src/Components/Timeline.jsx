be1ae6107b37dee6afce3f75bda5202223ac007f
PovilasU <povilas.urbonas85@gmail.com>
2022-02-08 15:58:42 +0100

    Created Timeline and timelineItem components

---
+++ src/Components/Timeline.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TimelineItem from './TimelineItem';
+const Timeline = () => {
+  return (
+    <div>
+      <p>Timeline component</p>
+      <TimelineItem />
+    </div>
+  );
+};
+
+export default Timeline;
---
+++ src/Components/TimelineItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const TimelineItem = () => {
+  return (
+    <div>
+      <p>Timeline Item</p>
+    </div>
+  );
+};
+
+export default TimelineItem;
