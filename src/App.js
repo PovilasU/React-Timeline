c9e18c7055a55bd9a3f1505cdcd487bca3a875a5
PovilasU <povilas.urbonas85@gmail.com>
2022-02-10 22:30:29 +0100

    Added link to git repo, wrote two tests to test header and link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Timeline from './Components/Timeline';
 function App() {
   return (
     <>
+      <div className="">
+        <a href="https://github.com/PovilasU/React-Timeline">
+          Link to git repository
+        </a>
+      </div>
       <h1>Events timeline</h1>
       <Timeline />
     </>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link to git repo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Link to git repository/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders header text', () => {
+  render(<App />);
+  const HeaderText = screen.getByText(/Events timeline/i);
+  expect(HeaderText).toBeInTheDocument();
 });
