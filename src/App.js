bcd4d0696f197d2b11d5e7b90ea060a45b994bca
PovilasU <povilas.urbonas85@gmail.com>
2022-02-11 11:37:29 +0100

    updated header and footer

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import Timeline from './Components/Timeline';
 function App() {
   return (
     <>
-      <div className="link-container">
-        <a href="https://github.com/PovilasU/React-Timeline">
-          Link to git repository
-        </a>
-      </div>
-      <h1>Events timeline</h1>
-      <h3>
-        Every 5 seconds new event is added to the timeline and last 5th event is
-        removed
-      </h3>
+      <header>
+        <h1>Events timeline</h1>
+        <h4>
+          Every 5 seconds a new event is added on top of the timeline and last
+          5th event is removed
+        </h4>
+      </header>
+
       <Timeline />
+
+      <footer>
+        <h4>Povilas Urbonas (2022)</h4>
+        <div className="link-container">
+          <a href="https://github.com/PovilasU/React-Timeline">
+            Link to git repository
+          </a>
+        </div>
+      </footer>
     </>
   );
 }
